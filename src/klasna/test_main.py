0780b339c627e0ddab00b7ff6c01ea1c6632e7bf
tanjiro <viktor1svider@gmail.com>
2026-08-04 14:03:53 +0000

    add test for studentParent unlink

--- src/klasna/test_main.py
+++ src/klasna/test_main.py
@@ -182,3 +182,38 @@ def test_student_parent_link(created_student, created_parent):
     post_response = client.post(f"{baseurl}/students/{student_id}/parents/{parent_id}")
     assert post_response.status_code == status.HTTP_200_OK
     assert created_parent in post_response.json()["parents"]
+
+
+def test_student_parent_unlink(created_student, created_parent):
+    student_id = created_student["id"]
+    parent_id = created_parent["id"]
+    print(created_parent)
+    post_response = client.post(f"{baseurl}/students/{student_id}/parents/{parent_id}")
+    assert post_response.status_code == status.HTTP_200_OK
+    assert created_parent in post_response.json()["parents"]
+    delete_response = client.delete(
+        f"{baseurl}/students/{student_id}/parents/{parent_id}"
+    )
+
+    assert delete_response.status_code == status.HTTP_200_OK
+    assert created_parent in post_response.json()["parents"]
+    delete_response = client.delete(
+        f"{baseurl}/students/{student_id}/parents/{parent_id}"
+    )
+    get_response = client.get(f"{baseurl}/students/{student_id}/")
+    assert delete_response.status_code == status.HTTP_409_CONFLICT
+    assert created_parent not in get_response.json()["parents"]
+    delete_response = client.delete(
+        f"{baseurl}/students/{student_id}/parents/{999_999_999}"
+    )
+    assert delete_response.status_code == status.HTTP_404_NOT_FOUND
+
+    delete_response = client.delete(
+        f"{baseurl}/students/{999_999_999}/parents/{parent_id}"
+    )
+    assert delete_response.status_code == status.HTTP_404_NOT_FOUND
+
+    delete_response = client.delete(
+        f"{baseurl}/students/{999_999_999}/parents/{999_999_999}"
+    )
+    assert delete_response.status_code == status.HTTP_404_NOT_FOUND
